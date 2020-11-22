947e9bf067565471645e4320d9cecbe18e62883e
lau1944 <a954723219@gmail.com>
2020-11-22 11:41:42 +0800

    view binding

--- chatview/src/main/java/com/vau/chatview/ChatAdapter.kt
+++ chatview/src/main/java/com/vau/chatview/ChatAdapter.kt
@@ -4,30 +4,40 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SimpleAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.vau.chatview.databinding.MyChatBinding
+import com.vau.chatview.databinding.OtherChatBinding
 
-class ChatAdapter(list: List<Any>) : RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+class ChatAdapter(private val list: List<Any>) : RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+
+    lateinit var otherChatBinding: OtherChatBinding
+    lateinit var myChatBinding: MyChatBinding
 
     class ChatViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatAdapter.ChatViewHolder {
         return if (viewType == 0) {
-            ChatViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.other_chat, parent, false)
-            )
+            val layoutInflater = LayoutInflater.from(parent.context).inflate(R.layout.other_chat, parent, false)
+            otherChatBinding = OtherChatBinding.inflate(LayoutInflater.from(parent.context))
+            ChatViewHolder(layoutInflater)
         } else {
-            ChatViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.my_chat, parent, false)
-            )
+            val layoutInflater = LayoutInflater.from(parent.context).inflate(R.layout.my_chat, parent, false)
+            myChatBinding = MyChatBinding.inflate(LayoutInflater.from(parent.context))
+            ChatViewHolder(layoutInflater)
         }
     }
 
-    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) = with(holder.itemView) {
+        if (holder.itemViewType == 0) {
+
+        } else {
+
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
 
